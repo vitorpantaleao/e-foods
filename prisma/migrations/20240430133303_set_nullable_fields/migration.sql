6afece74d1c8e92871380f5107b0adf5d2909080
Vitor Pantaleão <vitor-hmp@hotmail.com>
2024-04-30 10:39:42 -0300

    chore: add seed file and fix columns at schema

---
+++ prisma/migrations/20240430133303_set_nullable_fields/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE `restaurant` MODIFY `address` VARCHAR(191) NULL,
+    MODIFY `city` VARCHAR(191) NULL,
+    MODIFY `state` VARCHAR(191) NULL,
+    MODIFY `zip` VARCHAR(191) NULL,
+    MODIFY `phone` VARCHAR(191) NULL,
+    MODIFY `website` VARCHAR(191) NULL;
---
+++ prisma/migrations/20240430133805_fix_column_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `product` MODIFY `description` TEXT NOT NULL;
