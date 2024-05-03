b314f80c913d49d3d0536643a8fc167b7e33029d
Vitor Pantaleão <vitor-hmp@hotmail.com>
2024-05-03 00:31:44 -0300

    feat: add initial header component

---
+++ app/_components/header.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-between px-5 pt-6">
+      <Link href="/">
+        <Image src="/images/logo.png" alt="logo" width={100} height={30} />
+      </Link>
+      <Button
+        size="icon"
+        variant="outline"
+        className="border-none bg-transparent"
+      >
+        <MenuIcon />
+      </Button>
+    </div>
+  );
+};
+
+export default Header;
